f3cc2e2aed9ec0e3b62cfc23c7e731335f9891a5
Sophia Di <backwardsxmarathon@gmail.com>
2017-05-01 17:09:52 -0400

    Displays above search results the keywords and location that were searched for.

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ function YelpSearch() {
 	var keywordInput = document.getElementsByClassName('keyword-input')[0];
 	var searchBtn = document.getElementsByClassName('initiate-search-btn')[0];
 	var searchResults = document.getElementsByClassName('search-results__container')[0];
+	var searchedFor = document.getElementsByClassName('searched-for__container')[0];
+	var searchedForKeywords = document.getElementsByClassName('searched-for__keywords')[0];
+	var searchedForLocation = document.getElementsByClassName('searched-for__location')[0];
 
 	searchBtn.addEventListener("click", initiateSearch);
 	keywordInput.addEventListener("keyup", function(event) {
@@ -17,29 +20,35 @@ function YelpSearch() {
 	keywordInput.focus();
 
 	function initiateSearch() {
-		searchResults.innerHTML = "";
-		searchYelp(keywordInput.value, "Philadelphia");
+		var keywordStr = keywordInput.value;
+		var locationStr = "Philadelphia";
+		
+		keywordInput.value = "";
+
+		searchResults.innerHTML = "";		
+		searchYelp(keywordStr, locationStr);
+
+		searchedFor.style.display = "block";
+		searchedForKeywords.innerHTML = keywordStr;
+		searchedForLocation.innerHTML = locationStr;
 	}
 
 	function searchYelp(keywordStr, locationStr) {
 		$.ajax({
-	         url: "https://yelp-search.herokuapp.com/search",
-	         method: "GET",
-	         data: {
-	         	term: keywordStr,
-	         	location: locationStr
-	        },
-	         success: function(response) {
-	             console.log(response);
-	             var businesses = response.businesses;
-	             displaySearchResults(businesses);
-	         }
-
+	        url: "https://yelp-search.herokuapp.com/search",
+	        method: "GET",
+	        data: {
+	        	term: keywordStr,
+	        	location: locationStr
+	    	},
+	        success: function(response) {
+	            var businesses = response.businesses;
+	            displaySearchResults(businesses);
+	        }
 	    });
 	}
 
 	function displaySearchResults(response) {
-		console.log(response);
 
 		response.forEach(function(business, index) {
 			var newDiv = "<div class='search-results__item'>";
