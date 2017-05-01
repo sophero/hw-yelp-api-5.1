6b51d1f16c4ebf8bbbcb911109a83ebecac4a6e7
Sophia Di <backwardsxmarathon@gmail.com>
2017-05-01 16:18:44 -0400

    Adds address info to search results display.

--- script.js
+++ script.js
@@ -26,14 +26,34 @@ function YelpSearch() {
 	function displaySearchResults(response) {
 		console.log(response);
 
-		response.forEach(function(business) {
-			var newDiv = "<div class='search-results__item'><a target='_blank' href='";
-			newDiv += business.url + "'>";
-			newDiv += business.name;
-			newDiv += "</a>";
+		response.forEach(function(business, index) {
+			var newDiv = "<div class='search-results__item'>";
+			// <div class='search-results__item__image'></div>
+			var anchorTag = "<a target='_blank' href='";
+			anchorTag += business.url + "'>";
+			anchorTag += business.name;
+			anchorTag += "</a>";
+
+			var locationDiv = "<div class='search-results__item__location'>";
+			locationDiv += business.location.address[0] + ', ' + business.location.city;
+
+			newDiv += anchorTag + locationDiv + "</div>";
 
 			searchResults.innerHTML += newDiv;
+			
+			
+
 		});
+
+		// response.forEach(function(business, index) {
+		// 	var imageDiv = document.getElementsByClassName('search-results__item__image')[index];
+		// 	console.log(business.image_url);
+		// 	imageDiv.style.backgroundImage = business.image_url;
+		// 	console.log(document.getElementsByClassName('search-results__item__image'));
+			
+		// 	console.log(imageDiv);
+
+		// });
 
 
 		function newBusinessDiv () {				
